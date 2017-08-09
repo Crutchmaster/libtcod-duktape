eef11793e2d054265264481809860234116e3362
crutchmaster <crutchmaster@yandex.ru>
2017-08-10 03:49:44 +0700

    Game logic controller upgrade

--- js/sys.js
+++ js/sys.js
@@ -11,32 +11,36 @@ var Renderer = function() {
 }
 var Logic = function() {
     this.stack = [];
+    this.result = false;
     this.ctrl = {
         control : function(c,k) {}
     }
     this.add = function(i) {this.stack.push(i);}
     this.run = function() {
-            var stack = this.stack;
-            if (stack.length == 0) {
-                quit = true;
-            } else {
-                var node = stack[stack.length - 1];
-                this.ctrl = node;
-                var ret = node.run();
-                if (!ret) {
-                    stack.pop();
-                    if (stack.length == 0) {
-                        quit = true;
-                    } else {
-                        this.run();
-                    }
+        var stack = this.stack;
+        if (stack.length == 0) {
+            quit = true;
+        } else {
+            var node = stack[stack.length - 1];
+            this.ctrl = node;
+            var ret = node.run(this.result);
+            if (!ret) {
+                this.result = node.result;
+                stack.pop();
+                if (stack.length == 0) {
+                    quit = true;
                 } else {
-                    if (typeof(ret) == "object") {
-                        stack.push(ret);
-                        this.run();
-                    }
+                    this.run();
                 }
+            } else if (typeof(ret) == "object") {
+                stack.push(ret);
+                this.run();
+            } else if (typeof(ret) == "function") {
+                node.run = ret;
+                this.run();
             }
+        }
+        this.result = false;
     }
 }
 
@@ -46,39 +50,41 @@ var GameSelector = function() {
     this.control = function(c, k) {
         return this.menu.control(c, k);
     }
-    this.run = function() {
-        if (this.state == 0) {
-            var games_list = read_dir_list("js/");
-            for (var i = 0; i < games_list.length; i++) {
-                var s = games_list[i];
-                if (s == "." || s == "..") {
-                    games_list.splice(i,1);
-                    i--;
-                }
+    this.init = function() {
+        var games_list = read_dir_list("js/");
+        for (var i = 0; i < games_list.length; i++) {
+            var s = games_list[i];
+            if (s == "." || s == "..") {
+                games_list.splice(i,1);
+                i--;
             }
-            this.menu = new ui.menu(5,3,70,20, games_list);
-            this.state = 1;
         }
-        if (this.state == 1) {
-            this.menu.closed = false;
-            this.menu.index = -1;
-            render.add(this.menu);
-            this.state = 2;
-        }
-        if (this.state == 2) {
-            if (this.menu.closed) {
-                if (this.menu.index >=0 ) {
-                    var dirname = this.menu.list[this.menu.index];
-                    var Game = require("js/"+dirname+"/main");
-                    this.state = 1;
-                    return new Game();
-                } else {
-                    return false;
-                }
+        this.menu = new ui.menu(5,3,70,20, games_list);
+        return this.menuInit;
+    }
+
+    this.menuInit = function(ret) {
+        if (ret) print(ret);
+        this.menu.closed = false;
+        this.menu.index = -1;
+        render.add(this.menu);
+        return this.onMenuSelect;
+    }
+    this.onMenuSelect = function() {
+        if (this.menu.closed) {
+            if (this.menu.index >= 0 ) {
+                var dirname = this.menu.list[this.menu.index];
+                var Game = require("js/"+dirname+"/main");
+                this.run = this.menuInit;
+                return new Game();
+            } else {
+                return false;
             }
         }
         return true;
     }
-}
 
+
+    this.run = this.init;
+}
 module.exports = {Logic: Logic, Renderer: Renderer, GameSelector: GameSelector};
--- js/test_ui/main.js
+++ js/test_ui/main.js
@@ -1,24 +1,22 @@
 var menuTest = function() {
-    this.state = 0;
+    this.result = false;
     this.menu = {};
     this.control = function(c, k) {
         return this.menu.control(c, k);
     }
-    this.run = function() {
-        var state = this.state;
-        if (state == 0) {
-            this.menu = new ui.menu(3,3,10,5,["a","b","c","e","f","g","h"]);
-            render.add(this.menu);
-            state = 1;
+    this.init = function() {
+        this.menu = new ui.menu(3,3,10,5,["a","b","c","e","f","g","h"]);
+        render.add(this.menu);
+        return this.menuWork;
+    }
+    this.menuWork = function() {
+        if (this.menu.closed) {
+            this.result = this.menu.idx;
+            return false;
         }
-        if (state == 1) {
-            if (this.menu.closed) {
-                return false;
-            }
-        }
-        this.state = state;
         return true;
     }
+    this.run = this.init;
 }
 
 module.exports = menuTest;
